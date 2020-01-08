Added more entropy regularization terms. Fixed classifier by adding Gumbel-Softmax sampling.

DRIMseq.py
@@ -57,6 +57,13 @@ def is_float(x):
 def is_tensor(x):
     return isinstance(x, torch.FloatTensor) or isinstance(x, torch.Tensor)
 
+def vectorized_multinomial(prob_matrix):
+    items = np.arange(prob_matrix.shape[1])
+    s = prob_matrix.cumsum(axis=1)
+    r = np.random.rand(prob_matrix.shape[0],1)
+    k = (s < r).sum(axis=1)
+    return items[k]
+
 ###########################################################################
 #
 #                               Classes
@@ -77,22 +84,29 @@ class Agent:
                             'alpha': {
                                         'sl': 1.0,
                                         'ql': 1.0,
-                                        'cl': 1e-2
+                                        'cl': 1e-6
+                                    },
+                            'beta': {
+                                        'H(S|s)': 1e-20,
+                                        'H(S|R;T)': 1e-20,
+                                        'H(nS|S,A,T)': 1e-20,
+                                        'H(S|T)': 1e-20
                                     },
                             'init_epsilon': 0.00,
                             'min_epsilon': 0.00,
                             'delta_epsilon': 1.6e-6,
                             'epsilon_tl': 0.05,
                             'init_threshold_entropy_alpha': 0.0,
+                            'init_threshold_entropy_alpha_cl': np.log(10),
                             'delta_threshold_entropy_alpha': 8e-6,
                             'delta_threshold_entropy_alpha_cl': 3.2e-6,
                             'min_threshold_entropy_alpha_ql': np.log(2),
                             'min_threshold_entropy_alpha_cl': np.log(2),
                             'DQN_learning_type': 'DQL',
                             'DQL_epsds_target_update': 6000,
-                            'init_beta': 0.4,
-                            'max_beta': 1.0,
-                            'delta_beta': 3.0e-4/4.0,
+                            # 'init_beta': 0.4,
+                            # 'max_beta': 1.0,
+                            # 'delta_beta': 3.0e-4/4.0,
                             
                             'lr': {
                                     'sl': {
@@ -109,7 +123,8 @@ class Agent:
                                             'v_target': 5e-3
                                         },
                                     'cl': {
-                                            'alpha': 3e-4
+                                            'alpha': 3e-2,
+                                            'beta': 3e-2
                                         }
                                     },
 
@@ -176,7 +191,7 @@ class Agent:
         self.threshold_entropy_alpha = {
                                         'sl': self.params['init_threshold_entropy_alpha'],
                                         'ql': self.params['init_threshold_entropy_alpha'],
-                                        'cl': np.log(self.n_concepts)
+                                        'cl': self.params['init_threshold_entropy_alpha_cl']
                                     }
         self.delta_threshold_entropy_alpha = self.params['delta_threshold_entropy_alpha']
         self.delta_threshold_entropy_alpha_cl = self.params['delta_threshold_entropy_alpha_cl']
@@ -186,14 +201,23 @@ class Agent:
         for learning_type in ['sl', 'ql']:
             self.alpha[learning_type] = (alpha[learning_type] * torch.ones(self.n_tasks[learning_type]).float().to(device) if is_float(alpha[learning_type]) else 
                             (alpha[learning_type].float().to(device) if is_tensor(alpha[learning_type]) else torch.from_numpy(alpha[learning_type]).float().to(device)))
+        beta = self.params['beta']
+        self.beta = {'H(S|s)': beta['H(S|s)']}
+        for entropy in ['H(S|R;T)', 'H(nS|S,A,T)', 'H(S|T)']:
+            if entropy in ['H(S|R;T)', 'H(S|T)']:
+                self.beta[entropy] = (beta[entropy] * torch.ones(self.n_tasks[learning_type]).float().to(device) if is_float(beta[entropy]) else 
+                                (beta[entropy].float().to(device) if is_tensor(beta[entropy]) else torch.from_numpy(beta[entropy]).float().to(device)))
+            else:
+                self.beta[entropy] = (beta[entropy] * torch.ones(self.n_tasks[learning_type], self.n_concepts, self.n_skills+1).float().to(device) if is_float(beta[entropy]) else 
+                                (beta[entropy].float().to(device) if is_tensor(beta[entropy]) else torch.from_numpy(beta[entropy]).float().to(device)))
 
         self.epsilon = self.params['init_epsilon']
         self.min_epsilon = self.params['min_epsilon']
         self.delta_epsilon = self.params['delta_epsilon']
         self.epsilon_tl = self.params['epsilon_tl']
-        self.beta = self.params['init_beta']
-        self.max_beta = self.params['max_beta']
-        self.delta_beta = self.params['delta_beta']
+        # self.beta = self.params['init_beta']
+        # self.max_beta = self.params['max_beta']
+        # self.delta_beta = self.params['delta_beta']
         
         # Nets and memory
         self.v = {
@@ -217,7 +241,7 @@ class Agent:
                         }
         
         self.actor = s_Net(self.n_skills, s_dim-(self.dim_excluded['init']+self.dim_excluded['last']), a_dim, lr=self.lr['sl']['pi']).to(device)
-        self.classifier = c_Net(self.n_concepts, s_dim-self.dim_excluded['middle'], self.n_skills+1, n_tasks=self.n_tasks['ql']).to(device)
+        self.classifier = c_Net(self.n_concepts, s_dim-self.dim_excluded['middle'], self.n_skills+1, n_tasks=self.n_tasks['ql'], lr=3e-5).to(device)
 
         self.memory = {
                         'sl':  Memory(self.params['memory_capacity'], n_seed=self.seed),
@@ -230,7 +254,7 @@ class Agent:
             'Ntsas': np.zeros([self.n_tasks['tl'], self.n_concepts, self.n_skills+1, self.n_concepts]).astype(int)
         }
 
-        self.priority_queue = HeapPriorityQueue()
+        self.priority_queue = HeapPriorityQueue(self.n_tasks['tl'])
         
         # self.RNDnet = {
         #                 'sl': RND_Net(s_dim-(self.dim_excluded['init']+self.dim_excluded['last']), self.n_tasks['sl']).to(device),
@@ -578,41 +602,101 @@ class Agent:
 
         if batch_size > 0:
             s_batch = torch.FloatTensor(batch[:,self.dim_excluded['middle']:self.s_dim]).to(device)
-            r_batch = torch.FloatTensor(batch[:,self.s_dim+1]).view(-1,1).to(device)  
+            A_batch = batch[:,self.s_dim].astype('int')
+            r_batch = torch.FloatTensor(batch[:,self.s_dim+1]).view(-1).to(device)  
             ns_batch = torch.FloatTensor(batch[:,self.s_dim+2+self.dim_excluded['middle']:2*self.s_dim+2]).to(device)
             T_batch = batch[:,2*self.s_dim+3].astype('int')
 
             q = self.critic1['ql'](s_batch)[np.arange(batch_size), T_batch, :]
             A_batch_off = q.argmax(1)
 
+            A_batch_one_hot_off = torch.zeros(batch_size, self.n_skills+1).to(device)
+            A_batch_one_hot_off[np.arange(batch_size), A_batch_off] = torch.ones(batch_size,).to(device)
+            A_distribution_off = A_batch_one_hot_off.sum(0, keepdim=True)
+            A_distribution_off /= A_distribution_off.sum()
+
             A_batch_one_hot = torch.zeros(batch_size, self.n_skills+1).to(device)
-            A_batch_one_hot[np.arange(batch_size), A_batch_off] = torch.ones(batch_size,).to(device)
-            A_distribution = A_batch_one_hot.sum(0, keepdim=True)
-            A_distribution /= A_distribution.sum()
+            A_batch_one_hot[np.arange(batch_size), A_batch] = torch.ones(batch_size,).to(device)
 
             T_batch_one_hot = torch.zeros(batch_size, self.n_tasks['cl']).to(device)
             T_batch_one_hot[np.arange(batch_size), T_batch] = torch.ones(batch_size,).to(device)
 
-            A_predicted, PA_ST, log_PA_ST, S, PS_s, log_PS_s = self.classifier.sample_skills(s_batch, T_batch_one_hot)
-
+            A_predicted, PA_ST, log_PA_ST, S, PS_s, log_PS_s, z = self.classifier.sample_skills(s_batch, T_batch_one_hot)
             HS_s = -(PS_s * log_PS_s).sum(1).mean()
-            alpha_gradient = (HS_s - self.threshold_entropy_alpha['cl']).detach().item()            
-            classification_loss = (-log_PA_ST / ((self.n_skills + 1) * A_distribution.clamp(1.0/batch_size, 1.0)))[np.arange(batch_size), A_batch_off].mean()
 
-            classifier_loss = classification_loss + self.alpha['cl'] * HS_s if self.classification_with_entropies else classification_loss
+            PS_s_T = PS_s.unsqueeze(1) * T_batch_one_hot.unsqueeze(2)
+            ps_ST = PS_s_T / (PS_s_T.sum(0, keepdim=True) + 1e-10)
+            PS_T = PS_s_T.mean(0)
+            PS_T = PS_T / (PS_T.sum(1, keepdim=True) + 1e-10)
+            i_ST = Categorical(probs=ps_ST.transpose(0,1).transpose(1,2)).sample(sample_shape=torch.Size([batch_size//4]))
+            sigma = (r_batch.max() - r_batch.min()) / batch_size
+            R_iST = r_batch[i_ST] + sigma * torch.randn(batch_size//4, self.n_tasks['cl'], self.n_concepts).to(device)
+            log_pR_k_iST = -0.5 * (torch.log(2*np.pi*sigma**2+1e-10).view(1,1,1,1) + (R_iST.unsqueeze(1) - r_batch.view(1,-1,1,1))**2/sigma**2)
+            log_pR_pST_iS = torch.logsumexp(log_pR_k_iST.unsqueeze(4) + torch.log(ps_ST+1e-10).unsqueeze(0).unsqueeze(3), dim=1)
+            log_PR_ST_i = log_pR_pST_iS[:, :, np.arange(self.n_concepts), np.arange(self.n_concepts)]
+            log_PR_T_iS = torch.logsumexp(log_pR_pST_iS + torch.log(PS_T+1e-10).unsqueeze(0).unsqueeze(2), dim=3)
+            log_PS_RT = log_PR_ST_i + torch.log(PS_T+1e-10).unsqueeze(0) - log_PR_T_iS
+            HS_R_T = - (PS_T * log_PS_RT.mean(0)).sum(1, keepdim=True)
+
+            nS, PnS_ns, log_PnS_ns = self.classifier.sample_concepts(ns_batch)
+            AT_mask = T_batch_one_hot.unsqueeze(2) * A_batch_one_hot.unsqueeze(1)
+            PnS_S_iT = (PS_s.unsqueeze(2) * PnS_ns.unsqueeze(1)).unsqueeze(1) / (PS_T.unsqueeze(0).unsqueeze(3) + 1e-10)
+            PnS_SAT = torch.einsum('ihjl,ihk->hjkl', PnS_S_iT, AT_mask)
+            # N = PnS_SAT.sum(3, keepdim=True)
+            # N = N * (N.detach() > 0).float().to(device) + torch.ones_like(N.detach()) * (1 - (N.detach() > 0)).float().to(device)
+            PnS_SAT = PnS_SAT / (PnS_SAT.sum(3, keepdim=True) + 1e-10)
+            assert torch.all(PnS_SAT == PnS_SAT), 'EXPLOSION nS!'
+            HnS_SAT = -(PnS_SAT * torch.log(PnS_SAT + 1e-10)).sum(3) 
+
+            HS_T = -(PS_T * torch.log(PS_T + 1e-10)).sum(1, keepdim=True)
+            AT_mask_off = T_batch_one_hot.unsqueeze(2) * A_batch_one_hot_off.unsqueeze(1)
+            PA_T = AT_mask_off.sum(0)
+            # NA_T = PA_T.sum(1, keepdim=True)
+            # NA_T = NA_T * (NA_T.detach() > 0).float().to(device) + torch.ones_like(NA_T.detach()) * (1 - (NA_T.detach() > 0)).float().to(device)
+            PA_T = PA_T / (PA_T.sum(1, keepdim=True) + 1e-10)
+            assert torch.all(PA_T == PA_T), 'EXPLOSION A!'
+            HA_T = -(PA_T * torch.log(PA_T + 1e-10)).sum(1, keepdim=True)
+            
+            beta_Ss_gradient = (HS_s - self.threshold_entropy_alpha['cl']).detach().item()
+            beta_gradient_SR = (HS_R_T.squeeze(1) - self.threshold_entropy_alpha['cl']).detach()
+            beta_gradient_nSSAT = (HnS_SAT - self.threshold_entropy_alpha['cl']).detach()
+            beta_gradient_ST = (HA_T - HS_T).squeeze(1).detach()
+            classification_loss = (-log_PA_ST / ((self.n_skills + 1) * A_distribution_off.clamp(1.0/batch_size, 1.0)))[np.arange(batch_size), A_batch_off].mean()
+
+            classifier_loss = 1e+3*classification_loss
+            if self.classification_with_entropies:
+                classifier_loss += self.beta['H(S|s)'] * HS_s
+                classifier_loss += (self.beta['H(S|R;T)'].view(-1,1) * HS_R_T).mean()
+                classifier_loss += (self.beta['H(nS|S,A,T)'] * HnS_SAT).mean()
+                classifier_loss -= (self.beta['H(S|T)'].view(-1,1) * HS_T).mean()
+
             self.classifier.optimizer.zero_grad()
             classifier_loss.backward(retain_graph=True)
             clip_grad_norm_(self.classifier.parameters(), self.clip_value)
             self.classifier.optimizer.step()
+            assert torch.all(self.classifier.lv1e.weight==self.classifier.lv1e.weight), 'EXPLOSION c'
 
-            # Optimize dual variable  
-            log_alpha = np.log(self.alpha['cl'] + 1e-6)
-            log_alpha += self.lr['sl']['alpha'] * alpha_gradient
-            self.alpha['cl'] = np.exp(log_alpha).clip(1e-10, 1e+3)
+            # Optimize dual variables  
+            log_beta_Ss = np.log(self.beta['H(S|s)'])
+            log_beta_Ss += self.lr['cl']['beta'] * beta_Ss_gradient
+            self.beta['H(S|s)'] = np.exp(log_beta_Ss).clip(1e-20, 1e+2)
+
+            log_beta_SR = torch.log(self.beta['H(S|R;T)'])
+            log_beta_SR += self.lr['cl']['beta'] * beta_gradient_SR
+            self.beta['H(S|R;T)'] = torch.exp(log_beta_SR).clamp(1e-20, 1e+2)
+
+            log_beta_nSSAT = torch.log(self.beta['H(nS|S,A,T)'])
+            log_beta_nSSAT += self.lr['cl']['beta'] * beta_gradient_nSSAT
+            self.beta['H(nS|S,A,T)'] = torch.exp(log_beta_nSSAT).clamp(1e-20, 1e+2)
+
+            log_beta_ST = torch.log(self.beta['H(S|T)'])
+            log_beta_ST += self.lr['cl']['beta'] * beta_gradient_ST
+            self.beta['H(S|T)'] = torch.exp(log_beta_ST).clamp(1e-20, 1e+2)
 
             self.threshold_entropy_alpha['cl'] = np.max([self.threshold_entropy_alpha['cl'] - self.delta_threshold_entropy_alpha_cl, self.min_threshold_entropy_alpha['cl']])
         
-            return classifier_loss.detach().item(), HS_s.detach().item()
+            return(classification_loss.detach().item(), HS_s.detach().item(), HS_R_T.detach().mean().item(), 
+                    HnS_SAT.detach().mean().item(), (HS_T-HA_T).detach().mean().item()) 
         else:
             print("Here")
 
@@ -662,7 +746,9 @@ class Agent:
     
     def save(self, common_path, specific_path, learning_type):
         self.params['alpha'] = self.alpha
+        self.params['beta'] = self.beta
         self.params['init_threshold_entropy_alpha'] = self.threshold_entropy_alpha['sl']
+        self.params['init_threshold_entropy_alpha_cl'] = self.threshold_entropy_alpha['cl']
         self.params['init_epsilon'] = self.epsilon
         # self.params['init_density_C'] = self.density.C
         
@@ -1027,13 +1113,16 @@ class System:
         suffix = '_we' if self.agent.classification_with_entropies else '_woe'
 
         for grad_step in range(0, self.steps['tr'][self.learning_type]):
-            loss, HS_s = self.agent.learn_concepts()
+            loss, HS_s, HS_RT, HnS_SAT, delta_H = self.agent.learn_concepts()
             losses.append(loss)
-            entropies.append(HS_s)
+            entropies.append([HS_s, HS_RT, HnS_SAT, delta_H])
 
             stdscr.addstr(0, 0, "Iteration: {}".format(grad_step))
             stdscr.addstr(1, 0, "Classification Loss: {}".format(np.round(loss, 4)))
             stdscr.addstr(2, 0, "Entropy H(S|s): {}".format(np.round(HS_s,4)))
+            stdscr.addstr(3, 0, "Entropy H(S|R;T): {}".format(np.round(HS_RT,4)))
+            stdscr.addstr(4, 0, "Entropy H(nS|S,A,T): {}".format(np.round(HnS_SAT,4)))
+            stdscr.addstr(5, 0, "Entropy diff. H(S|T)-H(A|T): {}".format(np.round(delta_H,4)))
             stdscr.refresh()
 
             if (grad_step + 1) % 50000 == 0:

nets_seq.py
@@ -366,13 +366,18 @@ class m_Net(nn.Module):
         self.l2 = nn.Linear(256, 256)
         self.l3 = nn.Linear(256, n_skills)  
 
+        self.bn1 = nn.BatchNorm1d(n_concepts+n_tasks) 
+        self.bn2 = nn.BatchNorm1d(256) 
+        self.bn3 = nn.BatchNorm1d(256) 
+
         self.apply(weights_init_) 
         
     def forward(self, S,T):
         x = torch.cat([S, T], 1)
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        x = torch.exp(self.l3(x))
+        x = F.relu(self.l1(self.bn1(x)))
+        x = F.relu(self.l2(self.bn2(x)))
+        x = self.l3(self.bn3(x))
+        x = torch.exp(x-x.max(1, keepdim=True)[0])
         PA_ST = x / x.sum(1, keepdim=True)
         return(PA_ST)
 
@@ -727,71 +732,73 @@ class multichannel_Linear(nn.Module):
 #
 #-------------------------------------------------------------
 class c_Net(nn.Module):
-    def __init__(self, n_concepts, s_dim, n_skills, n_tasks=1, lr=3e-4, vision_dim=20, vision_channels=3):
+    def __init__(self, n_concepts, s_dim, n_skills, n_tasks=1, lr=3e-4, vision_dim=20, vision_channels=3, tau=1.0):
         super().__init__()  
         self.n_tasks = n_tasks
         self.s_dim = s_dim   
         self.n_concepts = n_concepts
         self.vision_channels = vision_channels
         self.vision_dim = vision_dim
-        self.kinematic_dim = s_dim - vision_dim*vision_channels     
+        self.kinematic_dim = s_dim - vision_dim*vision_channels  
+        self.tau = tau   
         
         nc1 = vision_channels * 2
         nc2 = vision_channels * 4
         nc3 = vision_channels * 8
-        nc4 = vision_channels * 16
+        # nc4 = vision_channels * 16
 
         kernel_size1 = 4
         kernel_size2 = 4
         kernel_size3 = 3
-        kernel_size4 = 3
+        # kernel_size4 = 3
 
         dilation1 = 1
         dilation2 = 2
         dilation3 = 1
-        dilation4 = 2
+        # dilation4 = 2
         
-        k_dim1 = 24
-        k_dim2 = 20
-        k_dim3 = 15
-        k_dim4 = 10
+        k_dim1 = 20
+        k_dim2 = 15
+        k_dim3 = 10
+        # k_dim4 = 10
 
         v_dim1 = int((vision_dim - dilation1*(kernel_size1-1) - 1)/1 + 1)
         v_dim2 = int((v_dim1 - dilation2*(kernel_size2-1) - 1)/1 + 1)
         v_dim3 = int((v_dim2 - dilation3*(kernel_size3-1) - 1)/1 + 1)
-        v_dim4 = int((v_dim3 - dilation4*(kernel_size4-1) - 1)/1 + 1)
+        # v_dim4 = int((v_dim3 - dilation4*(kernel_size4-1) - 1)/1 + 1)
         
         self.lv1e = nn.Conv1d(vision_channels, nc1, kernel_size1, dilation=dilation1)
         self.lv2e = nn.Conv1d(nc1, nc2, kernel_size2, dilation=dilation2)
         self.lv3e = nn.Conv1d(nc2, nc3, kernel_size3, dilation=dilation3)
-        self.lv4e = nn.Conv1d(nc3, nc4, kernel_size4, dilation=dilation4)
+        # self.lv4e = nn.Conv1d(nc3, nc4, kernel_size4, dilation=dilation4)
         self.lv1g = nn.Conv1d(vision_channels, nc1, kernel_size1, dilation=dilation1)
         self.lv2g = nn.Conv1d(nc1, nc2, kernel_size2, dilation=dilation2)
         self.lv3g = nn.Conv1d(nc2, nc3, kernel_size3, dilation=dilation3)
-        self.lv4g = nn.Conv1d(nc3, nc4, kernel_size4, dilation=dilation4)        
+        # self.lv4g = nn.Conv1d(nc3, nc4, kernel_size4, dilation=dilation4)        
 
         self.lk1 = multichannel_Linear(1, nc1, self.kinematic_dim, k_dim1)
         self.lk2 = multichannel_Linear(nc1, nc2, k_dim1, k_dim2)
         self.lk3 = multichannel_Linear(nc2, nc3, k_dim2, k_dim3)
-        self.lk4 = multichannel_Linear(nc3, nc4, k_dim3, k_dim4)
+        # self.lk4 = multichannel_Linear(nc3, nc4, k_dim3, k_dim4)
 
-        self.lc1x1 = nn.Conv1d(nc4, n_concepts, 1, stride=1)
-        self.lkv = parallel_Linear(n_concepts, v_dim4+k_dim4, 1)
+        self.lc1x1 = nn.Conv1d(nc3, n_concepts, 1, stride=1)
+        self.lkv = parallel_Linear(n_concepts, v_dim3+k_dim3, 1)
+        self.lkv2 = nn.Linear(n_concepts, n_concepts)
         
-        self.bnv1 = nn.BatchNorm1d(nc1)
-        self.bnv2 = nn.BatchNorm1d(nc2)
-        self.bnv3 = nn.BatchNorm1d(nc3)
-        self.bnv4 = nn.BatchNorm1d(nc4)
-        self.bnv1g = nn.BatchNorm1d(nc1)
-        self.bnv2g = nn.BatchNorm1d(nc2)
-        self.bnv3g = nn.BatchNorm1d(nc3)
-        self.bnv4g = nn.BatchNorm1d(nc4)
-        self.bnk1 = nn.BatchNorm1d(nc1)
-        self.bnk2 = nn.BatchNorm1d(nc2)
-        self.bnk3 = nn.BatchNorm1d(nc3)
-        self.bnk4 = nn.BatchNorm1d(nc4)
+        self.bnv1 = nn.BatchNorm1d(vision_channels)
+        self.bnv2 = nn.BatchNorm1d(nc1)
+        self.bnv3 = nn.BatchNorm1d(nc2)
+        # self.bnv4 = nn.BatchNorm1d(nc3)
+        self.bnv1g = nn.BatchNorm1d(vision_channels)
+        self.bnv2g = nn.BatchNorm1d(nc1)
+        self.bnv3g = nn.BatchNorm1d(nc2)
+        # self.bnv4g = nn.BatchNorm1d(nc3)
+        self.bnk1 = nn.BatchNorm1d(self.kinematic_dim)
+        self.bnk2 = nn.BatchNorm1d(nc1)
+        self.bnk3 = nn.BatchNorm1d(nc2)
+        # self.bnk4 = nn.BatchNorm1d(nc3)
 
-        self.bn5 = nn.BatchNorm1d(n_concepts)
+        self.bn4 = nn.BatchNorm1d(nc3)
 
         self.map = m_Net(n_concepts, n_skills, n_tasks)
                         
@@ -801,22 +808,25 @@ class c_Net(nn.Module):
 
     def forward(self, s):
         vision_input = s[:,-int(self.vision_dim*self.vision_channels):].view(s.size(0),self.vision_channels,self.vision_dim)
-        kinematic_input = s[:,:-int(self.vision_dim*self.vision_channels)].unsqueeze(1)
+        kinematic_input = s[:,:-int(self.vision_dim*self.vision_channels)]
 
-        v = torch.tanh(self.bnv1(self.lv1e(vision_input))) * torch.sigmoid(self.bnv1g(self.lv1g(vision_input)))
-        v = torch.tanh(self.bnv2(self.lv2e(v))) * torch.sigmoid(self.bnv2g(self.lv2g(v)))
-        v = torch.tanh(self.bnv3(self.lv3e(v))) * torch.sigmoid(self.bnv3g(self.lv3g(v)))
-        v = torch.tanh(self.bnv4(self.lv4e(v))) * torch.sigmoid(self.bnv4g(self.lv4g(v)))
+        v = torch.tanh(self.lv1e(self.bnv1(vision_input))) * torch.sigmoid(self.lv1g(self.bnv1g(vision_input)))
+        v = torch.tanh(self.lv2e(self.bnv2(v))) * torch.sigmoid(self.lv2g(self.bnv2g(v)))
+        v = torch.tanh(self.lv3e(self.bnv3(v))) * torch.sigmoid(self.lv3g(self.bnv3g(v)))
+        # v = torch.tanh(self.lv4e(self.bnv4(v))) * torch.sigmoid(self.lv4g(self.bnv4g(v)))
         
-        k = F.relu(self.bnk1(self.lk1(kinematic_input)))
-        k = F.relu(self.bnk2(self.lk2(k)))
-        k = F.relu(self.bnk3(self.lk3(k)))
-        k = torch.tanh(self.bnk4(self.lk4(k)))
+        k = F.relu(self.lk1(self.bnk1(kinematic_input).unsqueeze(1)))
+        k = F.relu(self.lk2(self.bnk2(k)))
+        k = F.relu(self.lk3(self.bnk3(k)))
+        # k = torch.tanh(self.lk4(self.bnk4(k)))
 
         x = torch.cat([k,v],2)
-        x = F.relu(self.bn5(self.lc1x1(x)))
-        x = torch.exp(self.lkv(x).squeeze(2))
+        x = F.relu(self.lc1x1(self.bn4(x)))
+        x = torch.sigmoid(self.lkv(x).squeeze(2))
+        x = self.lkv2(x)
+        x = torch.exp(x - x.max(1, keepdim=True)[0])
         PS_s = x / x.sum(1, keepdim=True)
+        assert torch.all(PS_s == PS_s), 'EXPLOSION PS!'
 
         return PS_s
     
@@ -830,9 +840,23 @@ class c_Net(nn.Module):
             S = Categorical(probs=tie_breaking_dist).sample().item()            
         return S, PS_s
 
+    def sample_differentiable_concepts(self, PS):
+        u = torch.rand_like(PS)
+        g = -torch.log((-torch.log(u+1e-10)).clamp(1e-10,1.0))
+        e = g + torch.log(PS+1e-10)
+        assert torch.all(e == e), 'EXPLOSION e!'
+        z = torch.zeros_like(PS)
+        z[np.arange(0,PS.shape[0]), e.argmax(1)] = torch.ones(PS.shape[0]).to(device)
+        y = torch.exp((e-e.max(1, keepdim=True)[0])/self.tau)
+        y = y / y.sum(1, keepdim=True)
+        assert torch.all(y == y), 'EXPLOSION y!'
+        z = z + y - y.detach()
+        return z
+
     def sample_skill(self, s, task, explore=True):
         S, PS = self.sample_concept(s, explore=explore)
-        PA_ST = self.map(PS, task)
+        z = self.sample_differentiable_concepts(PS)
+        PA_ST = self.map(z, task)
         if explore:
             A = Categorical(probs=PA_ST).sample().item()
         else:            
@@ -853,14 +877,17 @@ class c_Net(nn.Module):
 
     def sample_skills(self, s, task, explore=True):
         S, PS_s, log_PS_s = self.sample_concepts(s, explore=explore)
-        PA_ST = self.map(PS_s, task)
+        z = self.sample_differentiable_concepts(PS_s)
+        assert torch.all(z == z), 'EXPLOSION z!'
+        PA_ST = self.map(z, task)
+        assert torch.all(PA_ST == PA_ST), 'EXPLOSION map!'
         if explore:
             A = Categorical(probs=PA_ST).sample().cpu()
         else:            
             tie_breaking_dist = torch.isclose(PA_ST, PA_ST.max(1, keepdim=True)[0]).float()
             tie_breaking_dist /= tie_breaking_dist.sum()
             A = Categorical(probs=tie_breaking_dist).sample().cpu()
-        return A, PA_ST, torch.log(PA_ST+1e-12), S, PS_s, log_PS_s
+        return A, PA_ST, torch.log(PA_ST+1e-12), S, PS_s, log_PS_s, z
     
 class s_Net(nn.Module):
     def __init__(self, n_m_actions, input_dim, output_dim, min_log_stdev=-20, max_log_stdev=2, lr=3e-4, hidden_dim=256, 

train_DRIMseq.py
@@ -10,7 +10,7 @@ def exists_folder(f_name):
 n_test = 13
 last_iter_sl = 156
 last_iter_ql = 113
-last_iter_cl = 0
+last_iter_cl = 50000
 last_iter_tl = 0
 folder = 'Test'
 
@@ -27,13 +27,16 @@ concept_learning = True
 if (last_iter_sl + last_iter_ql + last_iter_cl) > 0:
     # try:
     params = pickle.load(open(path+'/params.p','rb'))
-    params['tr_steps_cl'] = 1200000
+    # params['tr_steps_cl'] = 1200000
     # params['env_names_tl'] = [
                             #     'AntCrossMaze-v3'
                             # ]
     # params['env_steps_tl'] = 100
     agent_params = pickle.load(open(path+'/agent_params.p','rb'))
-    agent_params['alpha']['cl'] = 1e-2
+    # agent_params.pop('beta', None)
+    # agent_params.pop('lr', None)
+    # agent_params.pop('init_threshold_entropy_alpha_cl', None)
+    # agent_params['alpha']['cl'] = 1e-2
     # agent_params['lr']['cl'] = {'alpha': 3e-4}
     print("Params loaded")
     if skill_learning or q_learning:  
@@ -49,24 +52,25 @@ if (last_iter_sl + last_iter_ql + last_iter_cl) > 0:
         metrics = []
     
     try:      
-        losses = list(np.loadtxt(path + '/concept_training_losses_we.txt'))     #TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO   
+        losses = list(np.loadtxt(path + '/concept_training_losses_we.txt')) if last_iter_cl > 0 else []     #TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO   
     except:
         losses = []
         
     try:      
-        entropies = list(np.loadtxt(path + '/concept_training_entropies_we.txt'))        
+        entropies = list(np.loadtxt(path + '/concept_training_entropies_we.txt')) if last_iter_cl > 0 else []       
     except:
         entropies = []
     
     print("Files loaded")
             
     system = System(params, agent_params=agent_params, skill_learning=skill_learning)
-    system.steps['tr']['cl'] = 1200000
-    system.agent.alpha['cl'] = 1e-2
+    # system.steps['tr']['cl'] = 1200000
+    # system.agent.alpha['cl'] = 1e-2
     # system.env_names['tl'] = [
     #                             'AntCrossMaze-v3'
     #                         ]
     # system.n_tasks['tl'] = 1
+    # system.agent.threshold_entropy_alpha['cl'] = np.log(10)
     print("System initialized")
     system.load(path, iter_0_sl=last_iter_sl, iter_0_ql=last_iter_ql, iter_0_cl=last_iter_cl, load_memory=(skill_learning or q_learning or concept_learning))
     print("Nets loaded")