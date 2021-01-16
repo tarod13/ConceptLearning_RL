ad58d0ea065c55b7c323ef1234c739cbcc8b2db9
Diego Gomez <df.gomez11@uniandes.edu.co>
2021-01-15 19:06:44 -0500

    Added parser to run training from command line

--- minimal_version/training_2l.py
+++ minimal_version/training_2l.py
@@ -10,20 +10,35 @@ from utils import numpy2torch as np2torch
 from wrappers import AntPixelWrapper
 
 import wandb
+import argparse
+
+DEFAULT_ENV_NAME = 'AntSquareWall-v3'
+DEFAULT_N_STEPS_IN_SECOND_LEVEL_EPISODE = 600
+DEFAULT_BUFFER_SIZE = 20000
+DEFAULT_N_EPISODES = 1500
 
 if __name__ == "__main__":    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cpu", default=False, action="store_true", help="Disable cuda")
+    parser.add_argument("--render", default=False, action="store_true", help="Display agent-env interaction")
+    parser.add_argument("--env", default=DEFAULT_ENV_NAME, help="Name of the environment, default=" + DEFAULT_ENV_NAME)
+    parser.add_argument("--nsteps", default=DEFAULT_N_STEPS_IN_SECOND_LEVEL_EPISODE, help="Number of second decision" +
+        "level steps taken in each episode, default=" + DEFAULT_METHOD)
+    parser.add_argument("--bsize", default=DEFAULT_BUFFER_SIZE, help="Size of replay buffer, default=" + DEFAULT_BUFFER_SIZE)
+    parser.add_argument("--nepsds", default=DEFAULT_N_EPISODES, help="Number of episodes, default=" + DEFAULT_N_EPISODES)
+    args = parser.parse_args()
+
     render_kwargs = {'pixels': {'width':168,
                             'height':84,
                             'camera_name':'front_camera'}}
-    render = True    
     MODEL_PATH = '/home/researcher/Diego/Concept_Learning_minimal/saved_models/'
     project_name = 'visualSAC_second_level'
     
     # Set hyperparameters
-    env_name = 'AntSquareWall-v3'
-    n_steps_in_second_level_episode = 600
-    buffer_size = 20000
-    n_episodes = 1500
+    env_name = args.env
+    n_steps_in_second_level_episode = args.nsteps
+    buffer_size = args.bsize
+    n_episodes = args.nepsds
 
     # Initilize Weights-and-Biases project
     wandb.init(project=project_name)
@@ -43,7 +58,7 @@ if __name__ == "__main__":
     database = PixelExperienceBuffer(buffer_size, level=2)
 
     trainer = Trainer(train_level=2)
-    returns = trainer.loop(env, agent, database, n_episodes=n_episodes, render=False, 
+    returns = trainer.loop(env, agent, database, n_episodes=n_episodes, render=args.render, 
                             max_episode_steps=n_steps_in_second_level_episode, 
                             store_video=False, wandb_project=True, MODEL_PATH=MODEL_PATH)
     G = returns.mean()    
